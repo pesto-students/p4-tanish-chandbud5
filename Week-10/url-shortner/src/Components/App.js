2370fda3714b3a5f2d1e167739ecc88cc2b00b87
chandbud5 <chandbud5@gmail.com>
2022-10-07 23:52:00 +0530

    Project Initialized

---
+++ Week-10/url-shortner/src/Components/App.js
@@ -0,0 +1,13 @@
+import '../CSS/App.css';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Shortner from './Shortner';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
