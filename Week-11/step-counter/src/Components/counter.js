20006b4c0610f39d611528194dfe8ab6594874e8
chandbud5 <chandbud5@gmail.com>
2022-10-15 16:38:36 +0530

    Added CSS to UI component

--- Week-11/step-counter/src/Components/counter.js
+++ Week-11/step-counter/src/Components/counter.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { mapStateToProps, mapDispatchToProps } from '../redux/steps/Dispatch'
+import '../CSS/counter.css'
 
 function Counter(props){
     return (
         <div>
-            <h1>Steps - {props.numOfSteps}</h1>
-            <button onClick={props.stepInc}>Add Steps</button>
-            <button onClick={props.stepReset}>Reset</button>
+            <p>You have walked {props.numOfSteps} steps today!</p>
+            <div className='controls'>
+                <button className='btn-purple-txt-white' onClick={props.stepInc}>Add Steps</button>
+                <button onClick={props.stepReset}>Reset</button>
+            </div>
         </div>
     )
 }
