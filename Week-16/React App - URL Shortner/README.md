23e8b4069dd754e4f6f542d3557fe53abb7c1f08
chandbud5 <chandbud5@gmail.com>
2022-11-29 09:27:28 +0530

    Added URL shortner project

---
+++ Week-16/React App - URL Shortner/README.md
@@ -0,0 +1,9 @@
+## URL Shortner
+
+We are using [shrtco.de](shrtco.de) API to shorten URL. Documentation for shrtco.de at [https://shrtco.de/docs/](https://shrtco.de/docs/).
+
+We are using post request to call API and get shortend URL.
+
+To generate build for this project - **npm run build**
+
+To run project on local server - **npm start**
---
+++ Week-16/React App - URL Shortner/src/Components/Shortner.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react'
+import '../CSS/shortner.css'
+import axios from "axios";
+
+// Loader
+const loaderAnimation = 'https://assets.materialup.com/uploads/b68f4460-aaa9-4e19-99d8-232dfea1c537/preview.gif'
+
+// To shorten URL using an API
+// link will store current value in input field
+// loader will help us decide whether to show loader animation or not
+// shortendURL will be the result we get returned from API
+function Shortner() {
+    const [link, setLink] = useState('');
+    const [loader, setLoader] = useState(false)
+    const [shortendURL, setShortendURL] = useState('');
+    const onChangeHandler = function (e) {
+        let temp = e.target.value;
+        setLink(temp);
+    };
+
+    // Calling API on clicking shorten button
+    const onClickShortner = function () {
+        console.log("Clicked")
+        setLoader(true)
+        if (link === "") {
+            console.log("Empty URL");
+            setLoader(false)
+            return;
+        }
+        axios.post(`https://api.shrtco.de/v2/shorten?url=${link}`)
+        .then((response)=>{
+            let data = response.data.result;
+            console.log(data)
+            setShortendURL(data.short_link)
+            setLink(data.original_link)
+            setLoader(false);
+            console.log(link, shortendURL)
+        });
+    };
+
+    // if loader is true then display loader animation
+    const Load = function (props){
+        console.log(props.loader)
+        if(props.loader){
+            return <img className='loader' src={loaderAnimation} alt='loader' />
+        }
+        else{
+            return ;
+        }
+    }
+
+    // Display will render the response we got from API
+    const Display = function (props){
+        console.log(props.link)
+        if(props.link !== ''){
+            return(
+                <div className='urlOutput'>
+                <button title='Copy' className='copy' onClick={()=>{
+                    navigator.clipboard.writeText(shortendURL);
+                }}><p className='shortendLink'>{shortendURL}</p></button>
+                </div>)
+        }
+    }
+    
+    return (
+    <div className='shortner'>
+        <div className='urlInput'>
+            <input onChange={onChangeHandler} placeholder='Shorten URL'></input>
+            <button onClick={onClickShortner}>Shorten</button>
+        </div>
+        <Load loader={loader}/>
+        <Display link={shortendURL} />
+    </div>
+  )
+}
+
+export default Shortner
