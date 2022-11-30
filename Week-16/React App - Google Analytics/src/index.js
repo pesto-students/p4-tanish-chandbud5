ee1ecc95f5bb9f9dc66597a043bd2e13128fe69e
chandbud5 <chandbud5@gmail.com>
2022-11-30 09:55:19 +0530

    Added React app for google analytics integration

---
+++ Week-16/React App - Google Analytics/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://065c74f39a014ab09a0ce5a55f93d623@o4504212214251520.ingest.sentry.io/4504212215562240",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
